00647e86be155fb6b6d9947bab39469f1f0aa003
Brian Labbe <brianlabbe@users.noreply.github.com>
2015-07-10 22:11:27 -0400

    Initial code commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+ * bl-logger 1.0.0
+ * ---------------
+ *		A simple logging library with multiple provided destinations and support for custom destinations
+ *
+ * 		(c) 2015 Brian Labbe
+ * 		bl-logger may be freely distributed under the MIT license.
+ */
+module.exports = require("./lib");
